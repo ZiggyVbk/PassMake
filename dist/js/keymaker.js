8ee7bd8fbfc8cea25fb535422df05920cb292233
Ziggy <ziggyv@live.nl>
2018-05-20 21:27:31 +0200

    Creates a list
    Also added some better (timer) functionality, css file indexed, html
    file redone and a password generation list of the last 4 generated
    passes.

--- dist/js/keymaker.js
+++ dist/js/keymaker.js
@@ -1,12 +1,17 @@
 "use strict";
-var keymaker = /** @class */ (function () {
-    function keymaker(progresselementname, codeoutputname, keylengthcount, debugon) {
+var keymaker = (function () {
+    function keymaker(progresselement, codeoutputelement, codelist, keylengthcount, timerms, debugon) {
         if (keylengthcount === void 0) { keylengthcount = 12; }
+        if (timerms === void 0) { timerms = 1000; }
         if (debugon === void 0) { debugon = false; }
+        this.keyarray = [];
+        this.keysgenerated = 0;
+        this.progressbar = progresselement;
+        this.codeoutput = codeoutputelement;
+        this.keylist = codelist;
         this.keylength = keylengthcount;
+        this.timeoutms = timerms;
         this.debugstatus = debugon;
-        this.progressbar = progresselementname;
-        this.codeoutput = codeoutputname;
     }
     keymaker.prototype.genCharArray = function (charA, charZ) {
         var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
@@ -24,36 +29,64 @@ var keymaker = /** @class */ (function () {
         }
         return b;
     };
-    keymaker.prototype.progressBar = function () {
-        var progressval = Number(this.progressbar.value);
-        if (progressval == 0) {
-            progressval = 100;
-            this.codeoutput.innerHTML = this.generateNewKey(this.keylength, this.debugstatus);
+    keymaker.prototype.keyArrayHandler = function (passcode, storagelimit, debug) {
+        if (storagelimit === void 0) { storagelimit = 4; }
+        if (debug === void 0) { debug = false; }
+        if (this.keyarray.length >= storagelimit) {
+            this.keyarray.shift();
+            this.keyarray.push(passcode);
         }
         else {
-            progressval = progressval - 25;
+            this.keyarray.push(passcode);
         }
+        this.keylist.innerHTML = "";
+        for (var i = 0; i < this.keyarray.length; i++) {
+            this.keylist.innerHTML += this.keyarray[i] + "<br/>";
+        }
+        if (debug || this.debugstatus)
+            console.log(this.keyarray);
+    };
+    keymaker.prototype.progressBar = function (debug) {
+        if (debug === void 0) { debug = false; }
+        var progressval = Number(this.progressbar.value);
+        progressval = progressval - 25;
+        if (progressval == 0) {
+            this.codeoutput.innerHTML = this.generateNewKey(this.keylength, this.debugstatus);
+            progressval = 100;
+        }
+        if (debug || this.debugstatus)
+            console.log("progress: " + progressval);
         this.progressbar.value = String(progressval);
     };
-    keymaker.prototype.generateNewKey = function (keylength, debug) {
+    keymaker.prototype.startGeneratingKeys = function () {
+        var _this = this;
+        if (this.keysgenerated == 0)
+            this.codeoutput.innerHTML = this.generateNewKey();
+        this.generatetimer = setInterval(function () { return _this.progressBar(); }, this.timeoutms);
+    };
+    keymaker.prototype.stopGeneratingKeys = function () {
+        clearInterval(this.generatetimer);
+    };
+    keymaker.prototype.generateNewKey = function (keylength, insertkeyarray, debug) {
         if (keylength === void 0) { keylength = 12; }
+        if (insertkeyarray === void 0) { insertkeyarray = true; }
         if (debug === void 0) { debug = false; }
         var under = this.genCharArray('a', 'z'), upper = this.genCharArray('A', 'Z'), number = this.genNumberArray(), msg = "", a = 0, i = 0, c = [under, upper, number];
         for (i = 0; i < keylength; ++i) {
             a = Math.floor(Math.random() * c.length);
             msg += c[a][Math.floor(Math.random() * c[a].length)];
         }
+        if (insertkeyarray)
+            this.keyArrayHandler(msg, 4);
+        this.keysgenerated++;
         if (debug || this.debugstatus)
-            console.log(msg);
+            console.log("key: " + msg, "- keynr: " + this.keysgenerated);
         return msg;
     };
     return keymaker;
 }());
 window.onload = function () {
-    var progress = document.getElementById("progbar"), output = document.getElementById("keycode");
-    var passwordgen = new keymaker(progress, output);
-    window.setInterval(function () {
-        passwordgen.progressBar();
-    }, 1000);
-    output.innerHTML = passwordgen.generateNewKey();
+    var progress = document.getElementById("progbar"), output = document.getElementById("keycode"), keylist = document.getElementById("keycodelist");
+    var passwordgen = new keymaker(progress, output, keylist, 16, 2000);
+    passwordgen.startGeneratingKeys();
 };
--- src/keymaker.ts
+++ src/keymaker.ts
@@ -4,18 +4,30 @@ class keymaker {
 
     private progressbar : HTMLInputElement;
     private codeoutput : HTMLElement;
+    private keylist : HTMLElement;
+
+    private keyarray : Array<string> = [];
+    private keysgenerated : number = 0;
+
+    private generatetimer : any;
+    private timeoutms : number;
 
     private debugstatus : boolean;
 
-    constructor(progresselementname : HTMLInputElement, codeoutputname : HTMLElement, keylengthcount : number = 12, debugon : boolean = false) {
+    constructor(progresselement : HTMLInputElement, codeoutputelement : HTMLElement, codelist : HTMLElement, keylengthcount : number = 12, timerms : number = 1000, debugon : boolean = false) {
+        
+        this.progressbar = progresselement;
+        this.codeoutput = codeoutputelement;
+        this.keylist = codelist;
+
         this.keylength = keylengthcount;
+        
+        this.timeoutms = timerms;
 
         this.debugstatus = debugon;
-
-        this.progressbar = progresselementname;
-        this.codeoutput = codeoutputname;
     }
 
+    //Creates an ABC array
     private genCharArray(charA : string, charZ : string) {
         let a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
         for (; i <= j; ++i) {
@@ -23,6 +35,8 @@ class keymaker {
         }
         return a;
     }
+
+    //Creates a 0-9 array
     private genNumberArray(zero : number = 0, nine : number = 9) {
         let b = [], i = zero;
         for(; i <= nine; ++i)
@@ -32,37 +46,85 @@ class keymaker {
         return b;
     }
 
-    public progressBar() {
-        let progressval = Number(this.progressbar.value);	
-        if(progressval == 0)
+    //Removes the first by 4 passwords generated, pushing a new one in after OR just adds a new password
+    private keyArrayHandler(passcode : string, storagelimit : number = 4, debug : boolean = false)
+    {
+        if(this.keyarray.length >= storagelimit)
         {
-            progressval = 100;
-            this.codeoutput.innerHTML = this.generateNewKey(this.keylength, this.debugstatus);
+            this.keyarray.shift();
+            this.keyarray.push(passcode);
         }else{
-            progressval = progressval - 25;
+            this.keyarray.push(passcode);
         }
+
+        this.keylist.innerHTML = "";
+        for(let i = 0; i < this.keyarray.length; i++)
+        {
+            this.keylist.innerHTML += this.keyarray[i] + "<br/>";
+        }
+
+        if(debug || this.debugstatus) console.log(this.keyarray);
+    }
+
+    //Slices progress or resets it with a new code
+    private progressBar(debug : boolean = false) {
+        let progressval = Number(this.progressbar.value);	
+        
+        progressval = progressval - 25;
+        
+        if(progressval == 0){
+            this.codeoutput.innerHTML = this.generateNewKey(this.keylength, this.debugstatus); 
+            
+            progressval = 100; 
+        }
+        
+        if(debug || this.debugstatus) console.log("progress: " + progressval);
         this.progressbar.value = String(progressval);
     }
+
+    //Start the timer on the progressbar and let it more generate keys
+    public startGeneratingKeys()
+    {
+        if(this.keysgenerated == 0) this.codeoutput.innerHTML = this.generateNewKey();
+        this.generatetimer = setInterval(() => this.progressBar(), this.timeoutms);
+    }
+
+    //Stops the (timer/) whole generating business
+    public stopGeneratingKeys() {
+        clearInterval(this.generatetimer);
+    }
     
-    public generateNewKey(keylength : number = 12, debug : boolean = false) {
+    //Generates a new key (directly!)
+    public generateNewKey(keylength : number = 12, insertkeyarray : boolean = true, debug : boolean = false) {
         let under = this.genCharArray('a', 'z'), upper = this.genCharArray('A', 'Z'), number = this.genNumberArray(), msg = "", a = 0, i = 0, c = [under, upper, number];
+
         for(i = 0; i < keylength; ++i)
         {
             a = Math.floor(Math.random() * c.length);
             msg += c[a][Math.floor(Math.random() * c[a].length)];
         }
-        if(debug || this.debugstatus) console.log(msg);
+        
+        if(insertkeyarray) this.keyArrayHandler(msg, 4);
+        this.keysgenerated++;
+
+        if(debug || this.debugstatus) console.log("key: " + msg, "- keynr: " + this.keysgenerated);
         return msg;
     }
 }
 
+//Running the code:
 window.onload = () => {
     const   progress = <HTMLInputElement> document.getElementById("progbar"),
-            output = <HTMLElement> document.getElementById("keycode");
-    let     passwordgen = new keymaker(progress, output);
+            output = <HTMLElement> document.getElementById("keycode"),
+            keylist = <HTMLElement> document.getElementById("keycodelist");
     
-    window.setInterval(function() {
-        passwordgen.progressBar();   
-    }, 1000)
-    output.innerHTML = passwordgen.generateNewKey(); 
+    //Creates it
+    //let     passwordgen = new keymaker(progress, output, keylist, 12, 1000, true);
+    let     passwordgen = new keymaker(progress, output, keylist, 16, 2000);
+    
+    //Starts it
+    passwordgen.startGeneratingKeys();
+
+    //You can just generate one key
+    //output.innerHTML = passwordgen.generateNewKey(16, false); 
 };
